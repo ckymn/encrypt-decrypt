39a25da6e4c8c19e18820bb1d379224549a166b2
ckymn <cokyamanmuhammet@gmail.com>
2021-10-17 22:13:48 +0300

    aes code fixed

--- 2.week/aes.js
+++ 2.week/aes.js
@@ -1,29 +1,37 @@
 const aesjs = require("aes-js");
 const pbkdf2 = require("pbkdf2");
 
-const encrypt_text = (key_secret, iv_secret, text) => {
-    let key_256 = pbkdf2.pbkdf2Sync(key_secret, 'salt', 1, 32, 'sha512'),
-        iv_256 = pbkdf2.pbkdf2Sync(iv_secret, 'salt', 1, 16, 'sha512'),
-        key = new Array(...key_256),
-        iv = new Array(...iv_256);
+const encrypt = (key_secret, iv_secret, text) => {
+    let key_32 = pbkdf2.pbkdf2Sync(key_secret, 'salt', 1, 32, 'sha512'),
+        iv_16 = pbkdf2.pbkdf2Sync(iv_secret, 'salt', 1, 16, 'sha512'),
+        key = new Array(...key_32),
+        iv = new Array(...iv_16);
 
     let textBytes = aesjs.utils.utf8.toBytes(text);
     let aesCtr = new aesjs.ModeOfOperation.cfb(key, iv);
     let encryptedBytes = aesCtr.encrypt(textBytes);
     let encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-    aesjs.utils.hex.toBytes(encryptedHex);
+    let to_base64 = Buffer.from(encryptedHex, 'hex').toString("base64")
+    return console.log(to_base64);
+    
+}
+const decrypt = (key_secret, iv_secret, hash) => {
+    let key_32 = pbkdf2.pbkdf2Sync(key_secret, 'salt', 1, 32, 'sha512'),
+        iv_16 = pbkdf2.pbkdf2Sync(iv_secret, 'salt', 1, 16, 'sha512'),
+        key = new Array(...key_32),
+        iv = new Array(...iv_16);
+
+    let to_hex = Buffer.from(hash, "base64").toString("hex");
+    let encryptedBytes = aesjs.utils.hex.toBytes(to_hex);
     let decrpte_aesCtr = new aesjs.ModeOfOperation.cfb(key, iv);
     let decryptedBytes = decrpte_aesCtr.decrypt(encryptedBytes);
     let link = aesjs.utils.utf8.fromBytes(decryptedBytes);
     return console.log(link);
 }
-const decrypt_text = (hash) => {
-    let hash_to_text = Buffer.from(hash, 'base64').toString('ascii');
-    return console.log(hash_to_text);
-}
-let key = "rnop3TnHwJ7P9zzLb0Z3qUjfhu1Cx9bW";
-let iv = "YsiebTh0Sjr8dZKo";
-let text = "TextMustBeAMultipleOfSegmentSize"
-let hash = "YnVndW4gdmVyaWxlcm4gZGVnZXJsZXJpIGdpcmVjZWtzaW5peiBhcmthZGFzbGFyLg==";
-encrypt_text(key,iv,text);
-decrypt_text(hash);
+let key = "wEgDCNvhccofPTkFt9zUdDgZDIVdGC9L";
+let iv = "crGTopEfBGXE1k1x";
+let text = "http://yaz.tf.firat.edu.tr/tr"
+let hash = "mH0+F72gEa1Du2kLwzQthCapPAB3muYV5am8xjE=";
+
+encrypt(key, iv, text);
+decrypt(key, iv, hash);
--- 2.week/base64.js
+++
@@ -1,4 +0,0 @@
-let base64 = Buffer.from("Hello World").toString('base128')
-console.log(base64);
-let decrypt_base64 = Buffer.from("SGVsbG8gV29ybGQ=", 'base128').toString('ascii')
-console.log(decrypt_base64)
